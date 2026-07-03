4da89809b6ddcbc4b53ceac218f248bb58622c06
smitawaghh <smitawagh49@gmail.com>
2026-07-03 13:59:13 +0530

    Setup backend infrastructure with database and flyaway for now

---
+++ backend/src/test/java/com/smita/dairy/DairyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smita.dairy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
